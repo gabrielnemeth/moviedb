a752a3b04e8e27abfa7e3b19d52dd5377268a35f
Gabriel Nemeth <gabrielnemeth@hotmail.sk>
2020-11-30 18:06:13 +0100

    Use svg as logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { MovieListResult } from './interfaces/movie-list-result';
 import { fetchGenres } from './store/genre/genre.actions';
 import { State } from './store/state';
 import { fetchTrending as fetchTrending } from './store/trending/trending.actions';
-import { selectTrending } from './store/trending/trending.reducer';
 
 @Component({
   selector: 'app-root',
@@ -13,10 +10,6 @@ import { selectTrending } from './store/trending/trending.reducer';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  public movies$: Observable<MovieListResult[]> = this.store.select(
-    selectTrending
-  );
-
   public constructor(private store: Store<State>) {}
 
   public ngOnInit(): void {
