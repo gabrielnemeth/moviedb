97bfc6cda2abfb5798e6b8a4a3a3cfa30eba14a5
Gabriel Nemeth <gabrielnemeth@hotmail.sk>
2020-12-19 19:25:30 +0100

    Add cast with images only

--- src/app/components/media/media-detail/media-detail.component.ts
+++ src/app/components/media/media-detail/media-detail.component.ts
@@ -2,6 +2,10 @@ import { Component, Input } from '@angular/core';
 import * as moment from 'moment';
 import { MediaItem } from '../../../interfaces/media-item';
 import { isNil } from 'lodash-es';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+import { Observable, of } from 'rxjs';
+import { Cast } from '../../../interfaces/credits';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
     selector: 'app-media-detail',
@@ -9,11 +13,62 @@ import { isNil } from 'lodash-es';
     styleUrls: ['./media-detail.component.scss'],
 })
 export class MediaDetailComponent {
+    private _mediaItem: MediaItem | null;
+
     @Input()
-    public mediaItem: MediaItem | null;
+    public set mediaItem(item: MediaItem | null) {
+        this._mediaItem = item;
+        this.castWithProfileOnly$ = of(item).pipe(
+            filter((i) => !isNil(i?.credits)),
+            map((i) =>
+                i?.credits?.cast.filter((cast) => !isNil(cast.profile_path))
+            )
+        );
+    }
+
+    public get mediaItem(): MediaItem | null {
+        return this._mediaItem;
+    }
+
+    public castWithProfileOnly$: Observable<Cast[] | undefined>;
 
     public youtubeId: string;
     public openModal: boolean;
+
+    public config: SwiperConfigInterface = {
+        navigation: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            type: 'bullets',
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            480: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+            },
+            640: {
+                slidesPerView: 4,
+                spaceBetween: 20,
+            },
+            920: {
+                slidesPerView: 5,
+                spaceBetween: 20,
+            },
+            1024: {
+                slidesPerView: 6,
+                spaceBetween: 20,
+            },
+            1215: {
+                slidesPerView: 8,
+                spaceBetween: 20,
+            },
+        },
+    };
 
     public getStyle(
         imagePath: string | null | undefined
