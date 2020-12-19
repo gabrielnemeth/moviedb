ce5bc4c82102015fdf38d9f8bc86717c6a486413
Gabriel Nemeth <gabrielnemeth@hotmail.sk>
2020-12-19 18:22:55 +0100

    Create reusable placeholder component

--- src/app/components/media/media-item-placeholder-image/media-item-placeholder-image.component.ts
+++ src/app/components/media/media-item-placeholder-image/media-item-placeholder-image.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-media-item-placeholder-image',
     templateUrl: 'media-item-placeholder-image.component.html',
     styleUrls: ['media-item-placeholder-image.component.scss'],
 })
-export class MediaItemPlaceholderImageComponent {}
+export class MediaItemPlaceholderImageComponent {
+    @Input()
+    public iconClassNames = 'far fa-file-image';
+}
